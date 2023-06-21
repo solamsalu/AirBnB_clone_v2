c6edcb88dc884f85bfdd6709ae21cfdc8e36ffdb
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-06-21 15:21:31 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
-import os
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
 class Amenity(BaseModel, Base):
-    """Represents an amenity data set."""
+    '''The class for Amenity'''
     __tablename__ = 'amenities'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+
+    name = Column(String(128), nullable=False)
