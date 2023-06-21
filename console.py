7d6333b2c63853938c4a1d6fabb93d8b462fb0b6
Solomon Amsalu <74931480+solamsalu@users.noreply.github.com>
2023-06-21 14:51:27 +0300

    Update console.py

--- console.py
+++ console.py
@@ -230,9 +230,9 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
+            for k, v in objects(HBNBCommand.classes[args]).items():
+                # if k.split('.')[0] == args:
+                print_list.append(str(v))
         else:
             for k, v in objects.items():
                 print_list.append(str(v))
