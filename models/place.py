2d0d31cf481a2149284c3673d286596ef11e6fd1
pitzee <pitzee421@gmail.com>
2023-06-21 10:59:44 -0700

    revised

--- models/place.py
+++ models/place.py
@@ -4,6 +4,12 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from os import getenv
+#!/usr/bin/python3
+""" Place Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from os import getenv
 from models.amenity import Amenity
 
 place_amenity = Table('place_amenity', Base.metadata,
